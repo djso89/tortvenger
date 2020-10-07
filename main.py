91d120e6ca81762faa6e9bb95f597a2921550a8e
djso89 <danielso89@gmail.com>
2020-10-06 20:51:51 -0700

    initial commits

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+from objects import *
+
+PT1 = platform()
+P1 = Player()
+
+all_sprites = pygame.sprite.Group()
+all_sprites.add(PT1)
+all_sprites.add(P1)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    displaysurface.fill((0, 0, 0))
+
+    for entity in all_sprites:
+        displaysurface.blit(entity.surf, entity.rect)
+    P1.move()
+    pygame.display.update()
+    FramePerSec.tick(FPS)
