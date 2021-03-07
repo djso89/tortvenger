3697d753f9124e8439d16f73b536f8f69659883d
djso89 <danielso89@gmail.com>
2021-03-06 23:38:12 -0800

    added main driver

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
-from menus import main
+import subprocess, time
+file_path = 'menus/main.py'
+
+def main():
+    child = subprocess.Popen(['python3', file_path])
+    while child.poll() is None:
+        print("parent: child (pid = %d) is still running" % child.pid)
+        # do parent stuff
+        time.sleep(1)
+    print("parent: child has terminated, returncode = %d" % child.returncode)
+
+if __name__ == '__main__':
+    main()
