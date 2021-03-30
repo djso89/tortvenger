09d7e29ec59b9598b046b5143235b2001357c213
djso89 <danielso89@gmail.com>
2021-03-29 23:18:27 -0700

    modified generating cell: to place the cell in designated platform(cell_plat)

--- Battle/covid19.py
+++ Battle/covid19.py
@@ -6,6 +6,7 @@ from display import *
 from stage import *
 import random
 from spritesheet import SpriteSheet
+from stageobject import *
 
 NumCells = 10
 black = (0, 0, 0)
@@ -44,6 +45,8 @@ class COVID19(pygame.sprite.Sprite):
 
         #kinematic factors
         self.pos = vec(x, y)
+        self.start_x = 0
+        self.end_x = 0
         self.vel = vec(0, 0)
 
         # image frame
@@ -59,7 +62,10 @@ class COVID19(pygame.sprite.Sprite):
         # wait flag when cell get hit with player attack
         self.hitCell = False
         self.cnt_hc = 0
-
+        
+    def set_range(self, start_x, end_x):
+        self.start_x = start_x
+        self.end_x = end_x
 
     def place_cell(self, x, y):
         self.pos.x = x
@@ -74,12 +80,17 @@ class COVID19(pygame.sprite.Sprite):
             else:
                 self.image = self.dmg_cut[self.cnt_hc // period]
                 self.cnt_hc += 1
+                
 
     def move(self):
         """Make the cell move by itself """
-        if self.pos.x <= 0:
+        if self.pos.x < 0:
             self.direction = 0
-        elif self.pos.x >= 800:#WIN_W - self.image.get_width(): # (800):#
+        if self.pos.x <= self.start_x:
+            self.direction = 0
+        if self.pos.x >= self.end_x - self.rect.width : # (800):#
+            self.direction = 1
+        if self.pos.x >= WIN_W - self.rect.width:
             self.direction = 1
 
         if not self.hitCell:
@@ -110,14 +121,19 @@ Cells = pygame.sprite.Group()
 
 def cell_gen(numcells):
     for i in range (0, numcells, 1):
-        x = random.randrange(0, WIN_W - 100)
-        y = random.randrange(0, WIN_H - 200)
+        cp = random.choice(cell_plats.sprites())
+        x = random.randint(cp.rect.left, cp.rect.right)
+        x_ext_l = random.randrange(-200, 200, 25)
+        x_ext_r = random.randrange(0, 200, 25)
+        y = cp.rect.y
         cell = COVID19(x, y)
+        cell.place_cell(x, y - cell.image.get_height())
+        cell.set_range(cp.rect.left + x_ext_l, cp.rect.right + x_ext_r)
         Cells.add(cell)
     
 cell_gen(NumCells)
 
 C19 = COVID19(900, 500)
 C1 = COVID19(400,300)
-Cells.add(C19)
-Cells.add(C1)
+#Cells.add(C19)
+#Cells.add(C1)
