d5136e3059b40dd2bc943f908824918ab829a6f4
djso89 <danielso89@gmail.com>
2021-04-12 02:24:10 -0700

    empty __init__ file main directory

--- Battle/settings.py
+++ Battle/settings.py
@@ -5,6 +5,7 @@ import pygame
 class Settings:
 
     def __init__(self):
-        self.screen_width, self.screen_height = 1216, 640
+        self.screen_width = 1216
+        self.screen_height = 640
         self.bg_color = (225, 225, 225)
         self.flags = pygame.SCALED
