26cbe2338be537467a7326319859a6035e778f7a
djso89 <danielso89@gmail.com>
2023-07-30 00:26:56 -0700

    modified vid.py

--- Battle/troopakuppa19/vid.py
+++ Battle/troopakuppa19/vid.py
@@ -8,6 +8,7 @@ def play_cutscene_1_1():
     # Create a VideoCapture object and read from input file
 #    pygame.init()
     clip = VideoFileClip('videos/1_1.mp4')
-#    clipresized = clip.resize(height=600)
+
+    clipresized = clip.resize(height=600)
     clip.preview()
 #    pygame.quit()
